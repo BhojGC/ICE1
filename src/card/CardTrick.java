08aaae70db4f7af26072f3d822402aacbc2b0abf
BhojGC <ghartich@sheridancollege.ca>
2024-05-23 22:20:58 -0400

    version 1 - step 3 complete

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,6 +4,9 @@
  */
 package card;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -15,18 +18,51 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Random random = new Random();
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
+            Card c = new Card();          
+            c.setValue(random.nextInt(13)+1);
+            c.setSuit(Card.SUITS[random.nextInt(Card.SUITS.length)]);
+            
+            magicHand[i] = c;
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            System.out.println(c.getValue());
+            System.out.println(c.getSuit());
         }
+        Scanner scanner = new Scanner(System.in);
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+        System.out.println("Enter a Card Value Between (1-13): ");
+        int cardValue = scanner.nextInt();
+        
+        System.out.println("Enter a number for Suit between (0-3): ");
+        int cardSuit = scanner.nextInt();
+        boolean cardFound = false;
+        for(Card card :magicHand){
+            if (card.getValue() == cardValue && card.getSuit().equals(cardSuit)) {
+                cardFound = true;
+                break;
+            }
+        }
+        if(cardFound = true){
+            System.out.println("Bingo!! The Selected Card is in the Magic Hand");
+            
+        }else{
+            System.out.println("Sorry !! The Selected Card is not in the Magic Hand");
+        }
+                
+        
+             
+        scanner.close();         
+            
+ 
+               // add one luckcard hard code 2,clubs
+        }
     }
+
     
-}
+  
+
